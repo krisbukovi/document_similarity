4deffdd375287eb2496932b2b8fc18b9b6477da3
kris <bukovi@me.com>
2018-08-27 10:44:23 -0400

    create class def, almost done with pdf to text

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Document Similarity
 Current methods for document similarity all include processing the words in the document to change their representation into that of a vector. The distance between these two vectors is then calculated using one of the many difference equations.
 
-In my first attempt I will be using the Universal Sentence Encoder (research paper found here: https://arxiv.org/pdf/1803.11175.pdf) to create a fixed dimensional embedding representation of the sentences in the document to be processed. To process the pdfs to be text files I will be using PDFminer.six, the version of PDFminer compatible with Python 3. 
+In my first attempt I will be using the Universal Sentence Encoder (research paper found here: https://arxiv.org/pdf/1803.11175.pdf) to create a fixed dimensional embedding representation of the sentences in the document to be processed. To process the pdfs to be text files I will be using PDFminer.six, the version of PDFminer compatible with Python 3. (**note: had to run sed $'s/\r//' -i $(which pdf2txt.py) to fix error in pdf2txt.py file**)
+
--- document_similarity_1.py
+++ document_similarity_1.py
@@ -1,76 +1,120 @@
 # author: Kris Bukovi
 # last modified: August 26, 2018
 # purpose: Given 2 PDF documents, calculate the document similarity using the Universal Sentence Encoder
-# 
-#   
 
 import io
+# import tensorflow_hub as hub
+
 from pdfminer.converter import TextConverter
 from pdfminer.pdfinterp import PDFPageInterpreter
 from pdfminer.pdfinterp import PDFResourceManager
 from pdfminer.pdfpage import PDFPage
-import tensorflow_hub as hub 
+from pdfminer.layout import LAParams
 
 class SimilarityMap:
 
-    def importDoc(path):
+    # instance variable for list of paths to each pdf
+    def __init__(self, pdfList):
+
+        # initialize variable for list of paths 
+        self.pdfList = pdfList
+
+    def pdf2txt(self, size):
+
+        txtList = []
 
         # create instance of pdf resource manager
         rm = PDFResourceManager()
 
-        pdf = io.StringIO
+        # create an io file object (file function doesn't work here with pdfminer.six)
+        pdf = io.StringIO()
 
-        converter = TextConverter(rm, pdf)
+        codec = 'utf-8'
 
+        laparams = LAParams()
+
+        # create text converter
+        converter = TextConverter(rm, pdf, codec=codec, laparams=laparams)
+
+        # create instance of page interpreter 
         pg_interp = PDFPageInterpreter(rm, converter)
 
-        # convert pdf to txt file
-        
+        password = ""
 
+        maxpages = 0
 
-        # open document 
-        
+        pagenos = set()
 
-        # read document
-        
-        # store text in a df 
+        # loop through all files (can i ge rid of this loop?)
+        for i in range(size):
 
-        # return df  
+            # open file in read mode
+            with open(self.pdfList[i], 'rb') as inFile:
 
-    #clean data
-    def cleanData(df):
+                for page in PDFPage.get_pages(inFile, pagenos, maxpages=maxpages, password=password, caching = True, check_extractable = True):
+
+                    # pdf to text
+                    pg_interp.process_page(page)
+
+                # store text from pdf in variable 
+                text = pdf.getvalue()
+
+            # close converter and io file object
+            inFile.close()
+            converter.close()
+            pdf.close()
+
+            if text:
+                txtList.append(text)
+            else:
+                print("file %i is empty", i)
+
+            return txtList
+
+    # clean data
+    def cleanData(self, text):
+
         # go through text split where ending puncutation occurs to obtain sentences, store in vector
+        for i in range(len(text)):
+            text[i] = text[i].split(". ")
+
 
         # turn all letters in sentences to lower case
 
         # return vector
 
 
-    def createVector(v):
+    #def createVector(v):
 
         # create an Universal Sentence Encoder object 
-        embed = hub.Module("https://tfhub.dev/google/universal-sentence-encoder/1")
+    #    embed = hub.Module("https://tfhub.dev/google/universal-sentence-encoder/1")
 
         # 
-        embeddings = embed(v)
+    #    embeddings = embed(v)
 
-        print session.run(embeddings)
+    #    print session.run(embeddings)
 
 
 
         # return vector
 
     # calculate distance between two vectors
-    def calculateDistance(v1, v2):
+    #def calculateDistance(v1, v2):
 
 
 if __name__ == '__main__':
 
     # create list to store file paths for all PDFs
-    list = []
+    paths = []
 
     # get path to first file
-    s = String(input("Please enter the path for first file: "))
+    paths.append(str(input("Please enter the path for first file: ")))
+
+    simMap = SimilarityMap(paths)
+
+    simMap.pdf2txt(len(paths))
+
+
 
 
 
